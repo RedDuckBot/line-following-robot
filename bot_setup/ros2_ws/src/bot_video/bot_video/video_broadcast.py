348d6bd9d1c3ced25e7262c32bc1602ca310c225
RedDuckBot <dshillin11799@gmail.com>
2024-12-08 14:49:06 -0700

    Update video broadcast program

--- bot_setup/ros2_ws/src/bot_video/bot_video/video_broadcast.py
+++ bot_setup/ros2_ws/src/bot_video/bot_video/video_broadcast.py
@@ -2,19 +2,21 @@ from socket import *
 import pickle, cv2, sys
 
 """
-Program broadcasts live video of robots floor feed. Before calling this
-progam, pass host and port arguments for video server. 
+Program broadcasts live video for robot. Uses a file named clients.txt within
+the same directory to obtain the IPs for sending video stream to.
+
+Add a client to clients.txt file where each line has the following format:
+    [hostname] [port number]
 """ 
 
 def main():
-    host = sys.argv[0]
-    server_video_port = sys.argv[1]
+    clients = getClients()
 
     #Setup UDP socket
-    client_video_UDP_socket = socket(AF_INET, SOCK_DGRAM)
-    client_video_UDP_socket.setsockopt(SOL_SOCKET, SO_SNDBUF, 1000000)
+    host_video_UDP_socket = socket(AF_INET, SOCK_DGRAM)
+    host_video_UDP_socket.setsockopt(SOL_SOCKET, SO_SNDBUF, 1000000)
    
-    print("[video_thread] Ready for live video stream")
+    print("Ready for live video stream")
 
     #Setup camera
     cap = cv2.VideoCapture(0)
@@ -22,12 +24,14 @@ def main():
     cap.set(4,480)
 
     while cap.isOpened():
-        ret, img = cap.read()
-        ret, buffer = cv2.imencode(".jpg", img, [int(cv2.IMWRITE_JPEG_QUALITY), 30])
+        _, img = cap.read()
+        _, buffer = cv2.imencode(".jpg", img, [int(cv2.IMWRITE_JPEG_QUALITY), 30])
 
         serial_buffer = pickle.dumps(buffer)
 
-        client_video_UDP_socket.sendto((serial_buffer),(host,server_video_port))
+        for client in clients:
+            host_video_UDP_socket.sendto((serial_buffer),
+                (client[0],client[1]))
 
         if cv2.waitKey(5) & 0xFF == 113:
             break
@@ -35,6 +39,33 @@ def main():
     cv2.destroyAllWindows()
     cap.release()
 
+def getClients() -> list:
+    """
+    Extract hosts' ip & port pairs from text file.
+
+    Returns:
+        list: Contains tuples of hostnames (str) & port numbers (int).
+    
+    Raises:
+        FileNotFoundError: If the file doesn't exist.
+    """
+    tempClientName = ""
+    tempClientPort = -1 
+    clients = []
+
+    try:
+        with open("clients.txt", "r") as fil:
+            for line in fil: 
+                pair = line.split()
+                namePortPair = (pair[0],int(pair[1]))
+                clients.append(namePortPair)
+
+    except FileNotFoundError:
+        raise FileNotFoundError("Clients file not found.")
+
+    return clients
+
+
+
 if __name__ == "__main__":
-    assert len(arg.v) == 2, "Two program arguments required, i.e. HOST & Port"
     main()
